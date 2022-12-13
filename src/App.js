140d18a660ea9fb4f85682fb3b2258e24c9c0eaa
Shokhmurod <shokhpjwstk@gmail.com>
2022-12-13 03:07:15 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import ExpenseItem from "./components/ExpenseItem";
-import logo from "./logo.svg";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 import "./App.css";
 
 function App() {
@@ -26,27 +26,8 @@ function App() {
   ];
   return (
     <div className="App">
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <NewExpense />
+      <Expenses item={expenses} />
     </div>
   );
 }
